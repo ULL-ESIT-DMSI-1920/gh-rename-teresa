80ff1e3d9f1762738e495cc423671cb39638f627
TeresaUll <91601345+TeresaUll@users.noreply.github.com>
2021-11-10 13:50:38 +0000

    clase 11/11

--- repo-rename-teresa.js
+++ repo-rename-teresa.js
@@ -8,12 +8,13 @@ const deb = (...args) => {
 const fs = require("fs");
 const shell = require('shelljs');
 const { program } = require('commander');
-const config = require("./package.json")
+const { args } = program;
+const { version }  = require("./package.json")
 
 program
-  .version(config.version)
-  .option('-r, --repo <type>', 'output extra debugging')
-  .option('-o, --org <type>', 'small pizza size')
+  .version(version)
+  .option('-r, --repo <type>', 'specifies the repo')
+  .option('-o, --org <type>', 'specifies the organization')
 
 program.parse(process.argv);
 
@@ -23,7 +24,7 @@ const options = program.opts();
 if (options.repo) console.log(options.repo);
 if (options.org) console.log(options.org);
 
-console.log(`program.args = ${deb(program.args)}`)
+console.log(`program.args = ${program.args}`)
 console.log("It is working")
 
 // comprobar que git y gh están instalados // 
@@ -37,8 +38,26 @@ if (!shell.which('gh')) {
   shell.exit(1);
 }
 
-const org = options.org;
-if (!org || !repo)  {
-  if (program.args.length < 2) program.help()
-
+let newName;
+if (!org) {
+  [org, repo] = args[0].split("/");
+  console.log(`org and repo ${org} ${repo}`);
+  console.log(`newName = ${newName}`)
 }
+if(!newName) newName = args[0]
+if(!org || !repo || newName) program.help()
+
+
+/*
+if (!repo) {
+  [org, repo] = args[1].split("/")
+}
+if ( args.length < 1) program.help();
+if (!org || !repo) {
+  program.help();
+}
+else if (args.length === 0) {
+  program.help();
+}
+console.log('The number of arguments is correct')
+*/
